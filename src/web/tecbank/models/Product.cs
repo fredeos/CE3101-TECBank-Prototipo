ab0c8f5e9a159efe04dc64c7c41745e3c5a1793e
fobando <fobando@estudiantec.cr>
2025-04-03 20:44:08 -0600

    backend v1

---
+++ src/web/tecbank/models/Product.cs
@@ -0,0 +1,10 @@
+namespace tecbank.models {
+    /// <summary>
+    /// hola
+    /// </summary>
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
